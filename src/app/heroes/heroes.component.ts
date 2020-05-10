bc0ce5f831419c4f99010f6973907661e2d872ef
HarshadGChavan <HarshadGChavan@yahoo.com>
2020-05-10 20:17:33 +0530

    Two way binding and interface

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../Hero';
 
 @Component({
   selector: 'app-heroes',
@@ -8,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class HeroesComponent implements OnInit {
 
   constructor() { }
+
+  // add property
+  hero: Hero = {
+    id: 1,
+    name: 'windstorm'
+  };
 
   ngOnInit(): void {
   }
